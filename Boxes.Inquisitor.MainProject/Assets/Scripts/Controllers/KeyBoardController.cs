fcf7bff377bf88a96bb82cdd6bb2b70532a247ff
Andreas Kielsgaard <andreas.kielsgaard@gmail.com>
2015-04-29 18:00:45 +0200

    Keyboard Controller 0.1
    Up, left, down and right controls added and linked to appropriate
    gamecontroller functions

---
+++ Boxes.Inquisitor.MainProject/Assets/Scripts/Controllers/KeyBoardController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoardController : MonoBehaviour {
+
+	public KeyCode up = KeyCode.UpArrow;
+	public KeyCode down = KeyCode.DownArrow;
+	public KeyCode left = KeyCode.LeftArrow;
+	public KeyCode right = KeyCode.RightArrow;
+	public GameController gc;
+
+
+	// Update is called once per frame
+	void Update () {
+		Controls ();
+	}
+
+	void Controls(){
+		if (Input.GetKey (up)) {
+			gc.MovePlayer(GameController.Direction.up);
+		}
+		else if (Input.GetKey (down)) {
+			gc.MovePlayer(GameController.Direction.down);
+		}
+		else if (Input.GetKey (left)) {
+			gc.MovePlayer(GameController.Direction.left);
+		}
+		else if (Input.GetKey (right)) {
+			gc.MovePlayer(GameController.Direction.right);
+		}
+	}
+}
