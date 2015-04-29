392622d27491be302bf2563a214841fb3a8968e4
Andreas Kielsgaard <andreas.kielsgaard@gmail.com>
2015-04-29 12:11:08 +0200

    Blocktype establishment
    Added the type Block

---
+++ Boxes.Inquisitor.MainProject/Assets/Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	SpriteRenderer visualiser;
+	public enum BlockType{background, player, collectible};
+	private BlockType type;
+
+	// Use this for initialization
+	void Start () {
+		throw new System.NotImplementedException ("The block type is not finished yet");
+	}
+	
+	void DisplayObject(){
+
+	}
+
+	Sprite GetSprite(BlockType type){
+		string basePath = "";
+		return null;
+	}
+
+	void setType(BlockType newType){
+		type = newType;
+		if(visualiser != null){
+			visualiser.sprite = GetSprite(type);
+		}
+	}
+
+
+}
