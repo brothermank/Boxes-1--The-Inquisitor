cca612a4d8243b211c025dd7496b90fbdcfd0281
Nikolaj Ignatieff Schwartzbach <ignatieff@outlook.com>
2015-05-05 20:30:41 +0200

    Finished AudioController
    it is still untested, though, but you can use it as:
    
    AudioController ac = new AudioController(AudioSource);
    ac.playSoundRandom(file-path-no-extension);

--- Boxes.Inquisitor.MainProject/Assets/Scripts/Controllers/AudioController.cs
+++ Boxes.Inquisitor.MainProject/Assets/Scripts/Controllers/AudioController.cs
@@ -2,12 +2,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 
 public class AudioController {
 
+	private AudioSource src;
 	private Dictionary<string, AudioClip> audio;
 
-	public AudioController(){
+	private double nextRandom(){
+		return UnityEngine.Random.Range (0f, 1f);
+	}
+
+	private float nextGaussian(double mean, double stdDev){
+		double u1 = nextRandom ();//these are uniform(0,1) random doubles
+		double u2 = nextRandom ();
+		double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+			Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+		return (float)(mean + stdDev * randStdNormal); //random normal(mean,stdDev^2)
+	}
+
+	private void playSound(string snd, double d){
+		playSound (snd, (float)d);
+	}
+
+	private void playSound(string snd, float pitch){
+		AudioClip a = null;
+		if (!audio.TryGetValue (snd, out a)) {
+			Debug.LogError("No such sound clip '"+snd+"'.");
+			return;
+		}
+
+		src.clip = a;
+		src.pitch = pitch;
+		src.Play ();
+	}
+
+	public void playSoundNormal(string snd){
+		playSound (snd, 1.0);
+	}
+	
+	public void playSoundRandom(string snd){
+		playSound (snd, nextGaussian(1.0,1.0));
+	}
+
+	public AudioController(AudioSource source){
 
 		audio = new Dictionary<string, AudioClip> ();
 
@@ -22,6 +60,9 @@ public class AudioController {
 
 			audio.Add(removeExtension(str), newAudio);
 		}
+
+		src = source;
+
 	}
 
 	private string removeExtension(string path){
